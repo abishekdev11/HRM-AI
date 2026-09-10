b89b8065d14f58eea748632bedf43900579af411
abishekdev11 <abishekdev1321@gmail.com>
2026-09-10 10:41:07 +0530

    done

--- frontend/src/api/chatbot.js
+++ frontend/src/api/chatbot.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://hrm-ai-backend-ltot.onrender.com",
 });
 
 // Add token to all requests
